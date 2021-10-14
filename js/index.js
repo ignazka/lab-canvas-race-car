c577415d8cf861e9cac48542dccf58e1c4fb127c
Philipp Kastl <picastle@posteo.de>
2021-10-14 20:25:48 +0200

    add iteration 1

--- js/index.js
+++ js/index.js
@@ -3,5 +3,16 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+
+    const img = new Image(); // Create new <img> element
+    img.src = '/images/road.png';
+
+    const ctx = document.querySelector('canvas').getContext('2d');
+
+    ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height)
+
+  }
 };
+
+
